ed857e8447a567fdfdfec32124a48e4e1419e09a
Leomaris Reyes <reyes.leomaris@gmail.com>
2022-09-20 23:41:19 -0500

    Update BuildingViewModel.cs

--- RealEstateFakeApp/BuildingViewModel.cs
+++ RealEstateFakeApp/BuildingViewModel.cs
@@ -16,8 +16,9 @@ namespace RealEstateFakeApp
         public BuildingViewModel()
         {
             buildingCollection = new ObservableCollection<Building>();
-            buildingCollection.Add(new Building { Picture = "room", Status = "Premium", StatusColor = Color.FromArgb("#ffa654"), Price = "$449,990", Description = "Exclusive, serviced, all bills included. City View", Beds = "2 beds", Bath = "2 baths", Sqfts = "150 sqfts", BuildingType = "Villa" });
+            buildingCollection.Add(new Building { Picture = "room2", Status = "Premium", StatusColor = Color.FromArgb("#ffa654"), Price = "$449,990", Description = "Exclusive, serviced, all bills included. City View", Beds = "2 beds", Bath = "2 baths", Sqfts = "150 sqfts", BuildingType = "Villa" });
             buildingCollection.Add(new Building { Picture = "room3", Status = "Featured", StatusColor = Color.FromArgb("#7bd944"), Price = "$449,990", Description = "Exclusive, serviced, all bills included. City View", Beds = "2 beds", Bath = "2 baths", Sqfts = "150 sqfts", BuildingType = "Villa" });
+    
         }
     }
 }
